8d967f570a3f3b61eb82fb5aec61544428b79461
Kevin Herzog <kevin.herzog@uni-ulm.de>
2022-12-16 10:26:16 +0100

    initial commit

---
+++ my_classes.py
@@ -0,0 +1,13 @@
+classes = [
+    ("pdp",'./uni/Paradigmen der Programmierung'), 
+    ("fg", './uni/Formale Grundlagen'), 
+    ("kn", './uni/Kombinatorik'),
+    ("ana2", './uni/Analysis 2'),
+    ("algo", './uni/Algorithmen und Datenstrukturen'),
+    ("adm", './uni/Angewandte Diskrete Mathematik'),
+    ("eidf", './uni/Einfuehrung in die Forschungsmethoden'),
+    ("es", './uni/Eingebettete Systeme'),
+    ("gdrn", './uni/Grundlagen der Rechnernetze'),
+    ("mathe1", './uni/Mathe 1'),
+    ("we", './uni/Web Engineering')
+]
---
+++ sorter.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import my_classes
+
+my_class = my_classes.classes
+
+for file in os.listdir('./files/'):
+    try:
+        filename = os.fsdecode(file)
+        source = f'./files/{filename}'
+        
+        for item in my_class:
+            if(item[0] in filename.lower()):
+                if not(os.path.exists(item[1])):
+                    os.mkdir(item[1])
+                destination = f'{item[1]}/{filename}'
+                match item[0]:
+                    case "kn":
+                        shutil.move(source, destination)
+                    case "pdp":
+                        shutil.move(source, destination)
+                    case "fg":
+                        shutil.move(source, destination)
+                    case "ana2":
+                        shutil.move(source, destination)
+                    case "algo":
+                        shutil.move(source, destination)
+                    case "adm":
+                        shutil.move(source, destination)
+                    case "eidf":
+                        shutil.move(source, destination)
+                    case "es":
+                        shutil.move(source, destination)
+                    case "gdrn":
+                        shutil.move(source, destination)
+                    case "we":
+                        shutil.move(source, destination)
+                    case "mathe1":
+                        shutil.move(source, destination)
+            continue
+        else:    
+            _, extension = os.path.splitext(filename)
+            destination = '/home/kevin/'
+            match extension:
+                case '.jpg' | '.jpeg' | '.png' | '.gif' | '.tiff':
+                    destination = destination + f'Pictures'
+                    shutil.move(source, destination)
+                case '.mp4' | '.avi':
+                    destination = destination + f'Videos'
+                    shutil.move(source, destination)
+                case '.pdf' | '.zip' | '.rar' | '.doc' | '.docx' | '.csv' | '.pptx' | '.ppt' | '.txt':
+                    destination = destination + f'Documents'
+                    shutil.move(source, destination)
+                case '.mp3':
+                    destination = destination + f'Music'
+                    shutil.move(source, destination)
+                case _:
+                    print("the case is not made for: " +filename)
+    except:
+        print("Das muss so, trust me bro")
